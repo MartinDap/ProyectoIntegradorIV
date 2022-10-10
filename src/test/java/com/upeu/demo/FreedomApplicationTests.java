41d89923298d9a914fbf63d8ea9c4ab6880f238b
MartinDap <jorge.del.aguila@upeu.edu.pe>
2022-10-09 21:22:46 -0500

    Subiendo Proyecto Integrador

---
+++ src/test/java/com/upeu/demo/FreedomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upeu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreedomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
